9548f7c014a7428a1c3ea36f523c6e4f086c4962
Landon Williams <lw05302021@gmail.com>
2024-01-23 14:04:40 -0600

    Basic UI complete

---
+++ README.md
@@ -0,0 +1,3 @@
+A small Python application that makes a noise when you speak too loudly into your microphone.
+
+Currently a work in progress.
--- main.py
+++ main.py
@@ -1,7 +1,28 @@
-import tkinter as tk
+\import tkinter as tk
+import tkinter.ttk as ttk
 
+def update_val(lbl):
+	out = "Current level: {:0.0f}%".format(float(lbl))
+	val.config(text = out)
+
+# main window
 window = tk.Tk()
-greeting = tk.Label(window,text="WORK YOU FUCK")
-greeting.pack()
+
+# used for scale later :)
+dbVal = tk.DoubleVar()
+
+# main elements of UI, subject to change
+content = ttk.Frame(window, width=600, height=400,borderwidth=5)
+scale = ttk.Scale(content, orient='vertical', length=200, from_=100, to=1, variable = dbVal, command=update_val)
+enable = ttk.Button(content, text="Enable")
+val = ttk.Label(content, text="Current level: 0%")
+
+content.grid(column=0, row=0)
+scale.grid(column=0, row=0, rowspan=2)
+enable.grid(column=1, row=0, rowspan=2)
+val.grid(column=0, row=3)
+
+#print(content.winfo_class())
+#print(style.layout('TFrame'))
 
 window.mainloop()
